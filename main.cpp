e74af685ba95d7be5e6efa44b33b7a84f9ee58d7
Ritham Sheti <sethiritham@gmail.com>
2026-03-26 19:22:57 +0530

    feat : successfully built my own allocater

--- main.cpp
+++ main.cpp
@@ -1,41 +1,116 @@
 #include <sys/mman.h>
 #include <list>
 #include<cstdint>
+#include<iostream>
 
 char* heap_ptr = (char*)mmap(nullptr, 65536, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-memID* free_list_head = (memID*)heap_ptr;
 
 typedef struct memID
 {
     size_t size;
     bool is_free = 0;
-};
+} memID;
+
+memID* free_list_head = (memID*)heap_ptr;
+static int called = 0;
 
 char* m_allocate(size_t size)
 {
+
+  if (size < 8) size = 8;
+
+  char* payload;
+  memID* prev;
+  memID* current;
+  memID* next_free;
+
+
+  if (!called) {
+
+    prev = nullptr;
+    current = free_list_head;
     
+    payload = (char*)free_list_head + sizeof(memID);
+
+    free_list_head->is_free = 1;
+    free_list_head->size = 65536 - sizeof(memID);
     size_t free_size = free_list_head->size;
 
-    memID* id = (memID*)free_list_head;
-    id->is_free = 0;
-    id->size = size;
+    current->is_free = 0;
+    current->size = size;
 
     free_list_head = (memID*)((char*)free_list_head + sizeof(memID) + size);
 
     free_list_head->is_free = 1;
     free_list_head->size = free_size - size - sizeof(memID);
 
+    next_free = nullptr; 
+    *(memID**)payload = next_free;
 
-    return (char*)id + sizeof(memID);
+    called = 1;
+    return (char*)current + sizeof(memID);
+  }
+
+  current = free_list_head;
+  prev = nullptr;
+
+
+  while(current!= nullptr && current->size < size)
+  {
+    prev = current;
+    current = next_free;
+    payload = (char*)current + sizeof(memID);
+
+    next_free = *(memID**)payload;  
+  }
+
+  if(current == nullptr) 
+  {
+    std::cout<<"NOT ENOUGH MEMORY!"<<std::endl; 
+    return nullptr;
+  }
+
+  int prev_size = current->size;
+    
+  current->size = size;
+  current->is_free = 0;
+ 
+  char* current_payload = (char*)current + sizeof(memID);
+    
+
+  memID* new_block =  (memID*)((char*)current + size + sizeof(memID));
+
+  new_block->size = prev_size - size - sizeof(memID);
+  new_block->is_free = 1;
+
+  if(prev == nullptr) 
+  {
+    free_list_head = new_block;
+    char* free_list_head_payload = (char*)free_list_head + sizeof(memID);
+    *(memID**)free_list_head_payload = next_free;
+  }
+
+  else
+  {
+    char* prev_payload = (char*)prev + sizeof(memID);
+    *(memID**)prev_payload = new_block;
+
+    char* new_block_payload = (char*)new_block + sizeof(memID);
+    *(memID**)new_block_payload = next_free;
+  }
+
+  return (char*)current + sizeof(memID);
 }
 
 int main()
 {
-    free_list_head->is_free = 1;
-    free_list_head->size = 65536 - sizeof(memID);
-    
-    heap_ptr = heap_ptr + sizeof(memID);
+  char* ptr = m_allocate(425);
+  char* ptr2 = m_allocate(300);
 
+  char x = 'A';
+  ptr = &x;
 
-    return 0;
+  std::cout<<"SUCESSFULLY ALLOCATED MEMORY "<<"\nVALUE OF VARIABLE IS "<<x<<std::endl;
+  return 0;
 }
+
